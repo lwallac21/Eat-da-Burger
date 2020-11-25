adda304e9d68d696df95b12d98aa096acc988a86
Lawrence Wallace <lwallac21@gmail.com>
2020-11-24 21:49:34 -0500

    FREAKING DID IT!

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ else{
 connection = mysql.createConnection({
     host: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "	lkpptjlfmzbpoc1n",
-    password: "	xfrb42i6x4er9h7j",
+    password: "	lkpptjlfmzbpoc1n",
     port:"3306",
     database: "l3vkgy69z9hgk43i"
 });
